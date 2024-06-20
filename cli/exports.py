a72ed3c1142227b246c94e6fc4c7e8b2d274d599
williamherring <william.herring.au@gmail.com>
2024-06-20 16:20:39 +1000

    started html export function

--- cli/exports.py
+++ cli/exports.py
@@ -1,5 +1,27 @@
+import markdown
+from os import listdir
+from os.path import isfile, join
+
 def get_pdf(html_file, outdir):
     pass
 
+
 def get_html(options, workingdir, outdir):
-    pass
+    title = options['metadata']['title']
+    pages_numbers_options = options['book']['page_numbers']
+    book = workingdir + '/book'
+
+    page_index = 0
+    # Pre-content
+    with open(book + '/pre-content.md', 'r') as file:
+        pages = file.read().split(repr("\\pg\\"))
+        for page in pages:
+            html = markdown.markdown(page, output_format='html')
+            out = open(outdir + f'/html/{page_index}')
+            out.write(html)
+            page_index += 1
+
+    # Chapters
+    chapter_files = [f for f in listdir(workingdir + '/book/chapters') if isfile(join(workingdir + '/book/chapters', f))]
+    for chapter in chapter_files:
+        break
